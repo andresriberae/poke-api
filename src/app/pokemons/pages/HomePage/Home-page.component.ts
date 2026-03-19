24e75083779c704c85950e120a6990508e2e7fd5
andresriberae <kcandres21@gmail.com>
2026-03-19 12:06:21 -0400

    design completed homepage

--- src/app/pokemons/pages/HomePage/Home-page.component.ts
+++ src/app/pokemons/pages/HomePage/Home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './Home-page.component.html',
 })
-export default class HomePageComponent { }
+export default class HomePageComponent {}
